15d18f5fe013bebc9ec8c6806d0f93e75029b15f
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-07-08 10:00:59 +0200

    Updated health-check script

--- applications/echo-client/health-check.sh
+++ applications/echo-client/health-check.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Kubernetes health check
 
-set -e
+# set -e
 
 HOST="$1"
 PORT=$2
-PATH="$3"
+ENDPOINT="$3"
 
-RESPONSE=$(curl --connect-timeout 2 --max-time 2 --write-out %{http_code} --silent --output /dev/null $HOST:$PORT/$PATH)
-# RESPONSE=$(/usr/bin/curl --connect-timeout 2 --max-time 2 --write-out %{http_code} --silent --output /dev/null $HOST:$PORT/$PATH)
-if [[ $RESPONSE == "200" ]]; then
+RESPONSE=$(curl --connect-timeout 2 --max-time 2 --write-out %{http_code} --silent --output /dev/null $HOST:$PORT/$ENDPOINT)
+if [[ $RESPONSE != "200" ]]; then
 	echo "KO :("
 	exit -1
 else
